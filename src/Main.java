206cdb8c94d45fca2aaf8b785e70bc30796386bc
OSushentsova <yaraindrop@gmail.com>
2026-01-04 21:39:34 +0300

    task 6.2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
 public class Main {
-    static Point point1 = new Point(1, 3);
-    static Point point2 = new Point(1, 3);
-    static Point point3 = new Point(1, 5);
-
     public static void main(String[] args) {
-        System.out.println("Точка 1: " + point1);
-        System.out.println("Точка 2: " + point2);
-        System.out.println("Точка 3: " + point3);
 
-        System.out.println("  point1 == point2: " + (point1 == point2));
-        System.out.println("  point1 == point3: " + (point1 == point3));
-        System.out.println("  point2 == point3: " + (point2 == point3));
+        Point point1 = new Point(1, 3);
+        Point point2 = new Point(5, 8);
+        Point point3 = new Point(10, 11);
+        Point point4 = new Point(15, 19);
+
+        Line line1 = new Line(point1, point2);
+        Line line2 = new Line(10, 11, 15, 19);
+        Line line3 = new Line(line1.end, line2.start);
+
+        System.out.println("Линия 3 до изменений: " + line3);
+
+        line1.end.x = 20;
+        line1.end.y = 25;
+
+        line2.start.x = 30;
+        line2.start.y = 35;
+
+        System.out.println("Линия 3 после изменений: " + line3);
+
+        double totalLength = line1.getLength() + line2.getLength() + line3.getLength();
+        System.out.println("Суммарная длина всех линий: " + totalLength);
+
     }
 }
--- src/Point.java
+++ src/Point.java
@@ -1,6 +1,6 @@
 class Point {
-    static int x;
-    static int y;
+    int x;
+    int y;
     Point(int x, int y) {
         this.x = x;
         this.y = y;
