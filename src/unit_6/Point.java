749583291ec21e2624781beb4a0411a19d7099cf
OSushentsova <yaraindrop@gmail.com>
2026-01-12 22:14:32 +0300

    task 6.1 fixed

---
+++ src/unit_6/Point.java
@@ -0,0 +1,14 @@
+package unit_6;
+
+class Point {
+    int x;
+    int y;
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+
+    }
+    public String toString() {
+        return "{" + x + ";" + y + "}";
+    }
+}
